84ecd51b5ac039ea9768af33f29a31cf5aa66d05
basilpai <basil@iMac-2.local>
2016-03-21 14:35:53 -0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$('.mainNav a').click( 
+  function(){
+    $('.hidden').fadeOut(500);
+    $( $(this).attr('href') ).fadeIn(750);
+  }
+)
+
+$('.btn-close').click(
+  function(){
+    $(this).closest('.content-box').fadeOut(500);
+  }
+)
